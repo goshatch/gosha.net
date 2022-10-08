176890739ee5189c573591bd317966abf5d6fc7b
Gosha Tcherednitchenko <git@gosha.net>
2022-10-09 00:09:30 +0100

    change: Improve theme toggle

--- assets/javascript/bundle.js
+++ assets/javascript/bundle.js
@@ -1,22 +1,29 @@
-// toggle to switch classes between .light and .dark
-// if no class is present (initial state), then assume current state based on system color scheme
-// if system color scheme is not supported, then assume current state is light
-//
-// Source: https://stackoverflow.com/a/68824350/12288625
-
-const toggleDarkMode = () => {
-  if (document.documentElement.classList.contains("light")) {
-    document.documentElement.classList.remove("light")
-    document.documentElement.classList.add("dark")
-  } else if (document.documentElement.classList.contains("dark")) {
-    document.documentElement.classList.remove("dark")
-    document.documentElement.classList.add("light")
+const setTheme = (lightOrDark) => {
+  if (lightOrDark == 'light') {
+    document.documentElement.classList.remove('dark')
+    document.documentElement.classList.add('light')
   } else {
+    document.documentElement.classList.remove('light')
+    document.documentElement.classList.add('dark')
+  }
+  localStorage.setItem('color-theme', lightOrDark)
+}
+
+const setInitialTheme = () => {
+  let theme = localStorage.getItem('color-theme');
+  if (!theme) {
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      document.documentElement.classList.add("light")
-    } else {
-      document.documentElement.classList.add("dark")
+      theme = 'dark'
     }
+  }
+  setTheme(theme || 'light')
+}
+
+const toggleTheme = () => {
+  if (document.documentElement.classList.contains("light")) {
+    setTheme('dark')
+  } else if (document.documentElement.classList.contains("dark")) {
+    setTheme('light')
   }
 }
 
@@ -24,9 +31,10 @@ const daysOfWar = () => {
   const warStart = new Date('2022-02-24')
   const currentDate = new Date()
 
-  return Math.round((currentDate-warStart)/(1000*60*60*24))
+  return Math.round((currentDate - warStart) / (1000 * 60 * 60 * 24))
 }
 
 (function() {
+  setInitialTheme()
   document.getElementById('war-days').innerHTML = `${daysOfWar()} days of war! `
 })()
