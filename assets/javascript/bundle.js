1075d05f4d664999b16dcb320afeda907aa7a57e
Gosha Tcherednitchenko <git@gosha.net>
2022-10-08 22:07:29 +0100

    new: Add light/dark theme toggle

---
+++ assets/javascript/bundle.js
@@ -0,0 +1,21 @@
+// toggle to switch classes between .light and .dark
+// if no class is present (initial state), then assume current state based on system color scheme
+// if system color scheme is not supported, then assume current state is light
+//
+// Source: https://stackoverflow.com/a/68824350/12288625
+
+function toggleDarkMode() {
+  if (document.documentElement.classList.contains("light")) {
+    document.documentElement.classList.remove("light")
+    document.documentElement.classList.add("dark")
+  } else if (document.documentElement.classList.contains("dark")) {
+    document.documentElement.classList.remove("dark")
+    document.documentElement.classList.add("light")
+  } else {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      document.documentElement.classList.add("light")
+    } else {
+      document.documentElement.classList.add("dark")
+    }
+  }
+}
