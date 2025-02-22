ac657fa88e8cf3c4bffa35e64d5c9289fba8a160
Gosha Tcherednitchenko <git@gosha.net>
2025-02-22 07:19:05 +0000

    feat(themes): temporarily remove theme toggle

--- assets/javascript/bundle.js
+++ assets/javascript/bundle.js
@@ -1,32 +1,3 @@
-const setTheme = (lightOrDark) => {
-  if (lightOrDark == 'light') {
-    document.documentElement.classList.remove('dark')
-    document.documentElement.classList.add('light')
-  } else {
-    document.documentElement.classList.remove('light')
-    document.documentElement.classList.add('dark')
-  }
-  localStorage.setItem('color-theme', lightOrDark)
-}
-
-const setInitialTheme = () => {
-  let theme = localStorage.getItem('color-theme');
-  if (!theme) {
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      theme = 'dark'
-    }
-  }
-  setTheme(theme || 'light')
-}
-
-const toggleTheme = () => {
-  if (document.documentElement.classList.contains("light")) {
-    setTheme('dark')
-  } else if (document.documentElement.classList.contains("dark")) {
-    setTheme('light')
-  }
-}
-
 const daysOfWar = () => {
   const warStart = new Date('2022-02-24')
   const currentDate = new Date()
@@ -35,6 +6,5 @@ const daysOfWar = () => {
 }
 
 (function() {
-  setInitialTheme()
   document.getElementById('war-days').innerHTML = `${daysOfWar()} days of war! `
 })()
